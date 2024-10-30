9e1d28e5e70d2998b8cb6c96bcb5ecb292955565
Virat-Shahi <virat.fullstack@gmail.com>
2024-10-30 14:17:09 +0700

    Added GuestHeader

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import GuestHeader from '../components/guest/GuestHeader'
 
 const GuestLayout = () => {
   return (
     <div>
+      <GuestHeader />
       <Outlet/>
+      
     </div>
   )
 }
