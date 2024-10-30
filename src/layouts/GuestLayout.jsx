c85633a229fb920fa3b2eb23403730f4679349ce
NattapolDedruktip <dedruktip.n@gmail.com>
2024-10-30 14:44:49 +0700

    add footer

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import GuestHeader from '../components/guest/GuestHeader'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import GuestHeader from "../components/guest/GuestHeader";
+import GuestFooter from "../components/guest/GuestFooter";
 
 const GuestLayout = () => {
   return (
     <div>
       <GuestHeader />
-      <Outlet/>
-      
+      <Outlet />
+      <GuestFooter />
     </div>
   );
 };
