24a640658a5ec8b308905b15043a3a75194d5b0c
SudtipongKn <166700563+Teamkn2001@users.noreply.github.com>
2024-11-12 14:57:19 +0700

    style: landing and dashboard

--- src/components/guest/GuestHeader.jsx
+++ src/components/guest/GuestHeader.jsx
@@ -9,24 +9,19 @@ const GuestHeader = () => {
             <div className='w-full h-[25px] bg-[#272988]'></div>
             <div className="text-[#272988]">
                 <div className="gap-6 items-center container mx-auto flex justify-center space-x-8 py-3 text-lg font-semibold">
-
                     <img
                         src="https://i.postimg.cc/mZnSzDB9/Group-7-Project.png"
                         alt="University Logo"
                         className="h-[125px] w-auto cursor-pointer"
                         onClick={() => navigate('/')}
                     />
-
-
                     <Link to="/about" className="text-2xl font-bold hover:text-black">ABOUT</Link>
                     <Link to="/academic" className="text-2xl font-bold hover:text-black">ACADEMIC</Link>
                     <Link to="/campus" className="text-2xl font-bold hover:text-black">CAMPUS</Link>
                     <Link to="/activities" className="text-2xl font-bold hover:text-black">ACTIVITIES</Link>
                     <Link to="/authentication" className="text-2xl font-bold  flex items-center gap-3 justify-center">LOGIN <LogIn size={28} /></Link>
-
                 </div>
             </div>
-
         </header >
     );
 }
