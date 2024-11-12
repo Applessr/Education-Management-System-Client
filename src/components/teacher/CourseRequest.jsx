d638fdbda2d98b8116503464f9d6e8f0de94ac91
Apple Srisura <sasita_ssr@kkumail.com>
2024-11-12 14:56:47 +0700

    edit

--- src/components/teacher/CourseRequest.jsx
+++ src/components/teacher/CourseRequest.jsx
@@ -4,13 +4,13 @@ import CourseRequestItem from "./CourseRequestItem";
 import useTeacher from "@/src/hooks/useTeacher";
 
 
-function CourseRequest() {
+const CourseRequest = () => {
   const { getEnrollRequest, enroll } = useTeacher();
   const token = localStorage.getItem("token");
 
   useEffect(() => {
     getEnrollRequest(token);
-  }, []);
+  }, [token]);
 
   console.log('enroll :>> ', enroll);
 
@@ -21,27 +21,25 @@ function CourseRequest() {
         );
         return {
           ...course,
-          pendingEnrollments,
+          enrollments: pendingEnrollments,
           pendingCount: pendingEnrollments.length,
         };
       }).filter(course => course.pendingCount > 0) 
     : [];
 
-
-
-
   return (
     <div>
-      {coursesWithPendingEnrollments?.map((course) => (
+      {coursesWithPendingEnrollments.map((course) => (
         <div className="mt-4" key={course.id}>
+          <h1 className="text-2xl font-bold text-[#b45309]">Enroll Request</h1>
           <CourseRequestItem
             course={course}
-            data={course.enrollments}
+            data={course.enrollments} 
           />
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default CourseRequest;
